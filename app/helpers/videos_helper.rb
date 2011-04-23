71f12e3153c981e2fffc13eefc163e3580abdabc
Kaid Wong <kaid@kaid.me>
2011-04-23 10:37:43 +0800

    trying youku parser

--- app/helpers/videos_helper.rb
+++ app/helpers/videos_helper.rb
@@ -1,2 +1,12 @@
 module VideosHelper
+  def parsed_video_url(v)
+    a = "http://www.flvcd.com/parse.php?kw=http://v.youku.com/v_show/id_#{v}&format=high"
+    doc = Nokogiri::HTML(open(a))
+    middle = nil
+    doc.xpath(%Q|//a[@class='link']|).each do |e|
+      middle = e['href']
+    end
+    rsp = Net::HTTP.get_response(URI.parse(middle))
+    rsp.header["location"]
+  end
 end
