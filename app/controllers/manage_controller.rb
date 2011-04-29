b1a0cf23c195c29224f2e51de290ba9736bf66a6
Kaid Wong <kaid@kaid.me>
2011-04-29 13:43:05 +0800

    and admin panel...

--- app/controllers/manage_controller.rb
+++ app/controllers/manage_controller.rb
@@ -3,8 +3,8 @@ class ManageController < ApplicationController
   before_filter :authenticate
 
   def home
-    @videos = Video.all
-    @artists = Artist.all
+    @videos = Video.all.sort {|a, b| b.created_at <=> a.created_at}
+    @artists = Artist.all.sort {|a, b| b.created_at <=> a.created_at}
     respond_to do |format|
       format.html
     end
