d9b8d94b6d2ac31c74e7429677f2ec34fbb90f57
Kaid Wong <kaid@kaid.me>
2011-05-11 14:08:34 +0800

    auto input video title and thumbnail

--- app/models/video.rb
+++ app/models/video.rb
@@ -4,6 +4,8 @@ class Video
   property :title, String
   property :video_token, String
   property :sub_on, Boolean, :default => false
+  property :thumb, String
+  property :auto_thumb, Boolean, :default => true
   timestamps!
 
   validates_presence_of :title
@@ -12,14 +14,15 @@ class Video
   many :artists
 
   before_create :short_key
-
-  def thumbnail
-    "thumbnails/#{self.key}.jpg"
-  end
+  before_save :default_thumb
 
 private
 
   def short_key
     self.key = rand(36**8).to_s(36)
+  end
+
+  def default_thumb
+    self.thumb = "/static/images/thumbnails/#{self.key}.jpg" unless self.auto_thumb
   end
 end
