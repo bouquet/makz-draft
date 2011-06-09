f80271fb31f647d7323489b69df32b15ea20f758
Kaid Wong <kaid@kaid.me>
2011-06-09 21:40:24 +0800

    ah, i'm always so careless

--- app/controllers/manage_controller.rb
+++ app/controllers/manage_controller.rb
@@ -5,6 +5,7 @@ class ManageController < ApplicationController
   def home
     @videos = Video.all.sort {|a, b| b.created_at <=> a.created_at}
     @artists = Artist.all.sort {|a, b| b.created_at <=> a.created_at}
+    @cymbals = Cymbal.all.sort {|a, b| b.created_at <=> a.created_at}
     respond_to do |format|
       format.html
     end
